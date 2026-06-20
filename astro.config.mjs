13ac764d104c04b3ec8298b0c7d28d67bd6e38b1
DeepakdevilB <deep.pgl.work@gmail.com>
2026-06-20 08:48:43 +0530

    Update domain to mypetagecalc.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://petagecalc.com',
+  site: 'https://mypetagecalc.com',
 
   vite: {
     plugins: [tailwindcss()],
